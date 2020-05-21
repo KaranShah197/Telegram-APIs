e079cb4043445c6210679e27f9325196b27e551f
KaranShah197 <karanshah197@gmail.com>
2020-05-20 20:59:40 -0400

    Spring Boot initial setup

---
+++ src/main/java/com/api/telegram/TelegramApplication.java
@@ -0,0 +1,14 @@
+package com.api.telegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TelegramApplication.class, args);
+	}
+
+}
